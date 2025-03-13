1cb7eaba332553356400d87fd86d91a52bc226b6
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-03-13 05:01:29 +0100

    fearure section added

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -9,8 +9,29 @@ const HeroSection = () => {
             <span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text ml-3">
                 for developers 
             </span>
-            
         </h1>
+        <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
+          Unleash your creativity and transform your VR app ideas into reality with our powerful development tools.  
+          Start building today and bring your imagination to life in an immersive world!
+        </p>
+        <div className="flex justify-center my-10">
+        <a href="#" className="bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 rounded-md mx-3">
+          Start for free
+        </a>
+        <a href="#" className="py-3 px-4 mx-3 border rounded-md">
+          Documentation
+        </a>
+        </div>
+        <div className="flex mt-10 justify-center">
+          <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4">
+            <source src={video1} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4">
+            <source src={video2} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
     </div>
   )
 }
